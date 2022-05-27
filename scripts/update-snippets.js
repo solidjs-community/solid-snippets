bb673ce53d284ca2e66104c484ebc90c8978f42c
Damian Tarnawski <gthetarnav@gmail.com>
2022-05-27 19:48:31 +0200

    update update-snippets script to update the package.json

--- scripts/update-snippets.js
+++ scripts/update-snippets.js
@@ -3,66 +3,83 @@ const { join } = require("path")
 
 const pathTo = (...path) => join(__dirname, ...path)
 
-// if (lang === "ts") languages.push("typescript", "typescriptreact")
-// else if (lang === "js") languages.push("javascript", "javascriptreact")
-// else languages.push("typescript", "typescriptreact", "javascript", "javascriptreact")
-
 const getCategoryName = prefix => prefix[0].toUpperCase() + prefix.substring(1)
 
 const getLanguageName = id => {
-	if (id === "ts") return "TS"
-	if (id === "js") return "JS"
-	return "TS & JS"
+  if (id === "ts") return "TS"
+  if (id === "js") return "JS"
+  return "TS & JS"
+}
+
+const getConfigLanguage = id => {
+  if (id === "ts") return ["typescript", "typescriptreact"]
+  if (id === "js") return ["javascript", "javascriptreact"]
+  return ["typescript", "typescriptreact", "javascript", "javascriptreact"]
 }
 
 const categories = {}
+const configList = []
 
 // PARSE SNIPPET FILES
 readdirSync(pathTo(`../snippets/`)).forEach(fileName => {
-	if (!fileName.endsWith(".json")) return console.log("Skipping unrecognized file:", fileName)
-	const json = JSON.parse(readFileSync(pathTo("../snippets/", fileName)))
+  if (!fileName.endsWith(".json")) return console.log("Skipping unrecognized file:", fileName)
+  const json = JSON.parse(readFileSync(pathTo("../snippets/", fileName)))
 
-	const [prefix, _lang] = fileName.split(".")
+  const [prefix, _lang] = fileName.split(".")
 
-	const lang = _lang === "ts" ? "ts" : _lang === "js" ? "js" : "*"
-	let snippets = categories[prefix]
-	if (!snippets) categories[prefix] = snippets = []
+  const lang = _lang === "ts" ? "ts" : _lang === "js" ? "js" : "*"
+  let snippets = categories[prefix]
+  if (!snippets) categories[prefix] = snippets = []
 
-	for (const snippetName in json) {
-		const { body, prefix, description } = json[snippetName]
-		if (typeof prefix !== "string" && !Array.isArray(prefix))
-			return console.log(`"${snippetName}" snippet in ${fileName} has incorrect prefix.`)
-		if (typeof body !== "string" && !Array.isArray(body))
-			return console.log(`"${snippetName}" snippet in ${fileName} has incorrect body.`)
-		if (typeof description !== "string")
-			return console.log(`"${snippetName}" snippet in ${fileName} has incorrect description.`)
+  for (const snippetName in json) {
+    const { body, prefix, description } = json[snippetName]
+    if (typeof prefix !== "string" && !Array.isArray(prefix))
+      return console.log(`"${snippetName}" snippet in ${fileName} has incorrect prefix.`)
+    if (typeof body !== "string" && !Array.isArray(body))
+      return console.log(`"${snippetName}" snippet in ${fileName} has incorrect body.`)
+    if (typeof description !== "string")
+      return console.log(`"${snippetName}" snippet in ${fileName} has incorrect description.`)
 
-		snippets.push({ snippetName, prefix, description, lang })
-	}
+    snippets.push({ snippetName, prefix, description, lang })
+  }
+
+  getConfigLanguage(lang).forEach(language => {
+    configList.push({
+      language,
+      path: `./snippets/${fileName}`,
+    })
+  })
 })
 
 // GENERATE SNIPPETS TABLE
 ;(() => {
-	const table = Object.entries(categories).reduce((md, [prefix, snippets]) => {
-		md += `|<h4>*${getCategoryName(prefix)}*</h4>|\n`
-		snippets.forEach(({ snippetName, prefix, description, lang }) => {
-			md += `|\`${prefix}\`|${snippetName}|${description}|${getLanguageName(lang)}|\n`
-		})
-		return md
-	}, "|Prefix|Name|Description|Languages|\n|----|----|----|----|\n")
+  const table = Object.entries(categories).reduce((md, [prefix, snippets]) => {
+    md += `|<h4>*${getCategoryName(prefix)}*</h4>|\n`
+    snippets.forEach(({ snippetName, prefix, description, lang }) => {
+      md += `|\`${prefix}\`|${snippetName}|${description}|${getLanguageName(lang)}|\n`
+    })
+    return md
+  }, "|Prefix|Name|Description|Languages|\n|----|----|----|----|\n")
 
-	const readme = readFileSync(pathTo("../README.md")).toString()
-	let start = readme.indexOf("-->", readme.indexOf("GENERATE-SNIPPETS-TABLE:START"))
-	if (start === -1)
-		return console.log("Couldn't find GENERATE-SNIPPETS-TABLE:START tag in README.md")
-	start += 3
+  const readme = readFileSync(pathTo("../README.md")).toString()
+  let start = readme.indexOf("-->", readme.indexOf("GENERATE-SNIPPETS-TABLE:START"))
+  if (start === -1)
+    return console.log("Couldn't find GENERATE-SNIPPETS-TABLE:START tag in README.md")
+  start += 3
 
-	const end = readme.indexOf("<!--", start)
-	if (end === -1) return console.log("Couldn't find GENERATE-SNIPPETS-TABLE:END tag in README.md")
+  const end = readme.indexOf("<!--", start)
+  if (end === -1) return console.log("Couldn't find GENERATE-SNIPPETS-TABLE:END tag in README.md")
 
-	let newReadme = readme.substring(0, start)
-	newReadme += `\n\n${table}\n`
-	newReadme += readme.substring(end)
+  let newReadme = readme.substring(0, start)
+  newReadme += `\n\n${table}\n`
+  newReadme += readme.substring(end)
 
-	writeFileSync(pathTo("../README.md"), newReadme)
+  writeFileSync(pathTo("../README.md"), newReadme)
+})()
+
+// UPDATE PACKAGE.JSON CONFIG
+;(() => {
+  const pkg = JSON.parse(readFileSync(pathTo("../package.json")))
+  pkg.contributes.snippets = configList
+  writeFileSync(pathTo("../package.json"), JSON.stringify(pkg, null, 2))
 })()
